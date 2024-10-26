d6684179a64229ca5ac28151b07624ba6d4ff9a4
Muhammad Umar Jan <muhammad.umar.jan11@gmail.com>
2024-10-25 18:56:40 -0700

    Advanced Data Structure
    A Binary Tree Data Structure is a hierarchical data structure in which each node has at most two children, referred to as the left child and the right child. It is commonly used in computer science for efficient storage and retrieval of data, with various operations such as insertion, deletion, and traversal.

---
+++ Binary Tree/Maxsumpath.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+struct Node{
+	int data;
+	struct Node* left;
+	struct Node* right;
+	Node(int val){
+		data = val;
+		left = NULL;
+		right = NULL;
+		
+	}
+};
+int maxsumpathutils(Node* root, int &ans){
+	if(root==NULL){
+		return 0;
+	}
+	int left = maxsumpathutils(root->left,ans);
+	int right = maxsumpathutils(root->right,ans);
+	int maxsum = max(max(root->data,root->data+left+right),max(root->data+left,root->data+right));
+    ans = max(ans,maxsum);
+    int singlepathsum= max(root->data,max(root->data+left,root->data+right));
+    return singlepathsum;
+}
+int maxsumpath(Node*root){
+	int ans = INT_MIN;
+	maxsumpathutils(root,ans);
+	return ans;
+}
+int main(){
+	Node* root = new Node(1);
+	root->left = new Node(2);
+	root->right = new Node(3);
+
+	cout<<maxsumpath(root);
+	
+}
